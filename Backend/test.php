0dced476a98f3edeba04f6b942b94939fe378e7a
csucla2015 <meetbhagdev@ucla.edu>
2015-10-03 21:47:04 -0700

    Update test.php

--- Backend/test.php
+++ Backend/test.php
@@ -1,1 +1,31 @@
- <?php echo '<p>Hello World</p>'; ?> 
+<?php
+
+	$data1 = array( 'a', 'b', 'c' );
+	$data2 = array( 'name' => 'God', 'age' => -1 );
+	$option = 1; 
+	header('Content-type: application/json');
+
+ 
+
+	$arr = array(
+    array(
+	        "name" => "Indian Food Truck",
+	        "votes" => "44",
+	        "menus" => array(
+	        	0 => array(
+	        			"id" => 1,
+	        			"item" => "Red Curry"),
+		        1 => array(
+	        			"id" => 2,
+	        			"item" => "Green Curry"),
+	    	    2 => array(
+	        			"id" => 3,
+	        			"item" => "Pananag Curry"),
+	    	),
+	    ),
+
+	);
+
+	echo json_encode($arr);
+
+?>
